6cb1507a6a71caa3af932bc9e940271b5c7449ac
Nilashish Roy <nilashishroyjoy@gmail.com>
2021-09-04 05:30:00 +0600

    Added Card

---
+++ app/src/main/java/com/example/interac/cards_layout.java
@@ -0,0 +1,14 @@
+package com.example.interac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cards_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cards_layout);
+    }
+}
