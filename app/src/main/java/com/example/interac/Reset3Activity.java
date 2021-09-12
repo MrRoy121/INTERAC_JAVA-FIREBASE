638c17cd7e2ea0b214778e426e98783c7a231854
Nilashish Roy <nilashishroyjoy@gmail.com>
2021-09-13 05:30:00 +0600

    Added Reset

---
+++ app/src/main/java/com/example/interac/Reset3Activity.java
@@ -0,0 +1,36 @@
+package com.example.interac;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Reset3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+        setContentView(R.layout.activity_reset3);
+        Button button9=(Button) findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Reset3Activity.this, LoginActivity.class);
+                startActivity(i);
+            }
+
+        });
+    }
+}
